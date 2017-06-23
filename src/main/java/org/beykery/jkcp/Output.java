edbcac01cf860e481bd206e8f031c24941809892
butfly <zbutfly@gmail.com>
2017-06-24 01:29:42 +0800

    format

--- src/main/java/org/beykery/jkcp/Output.java
+++ src/main/java/org/beykery/jkcp/Output.java
@@ -9,15 +9,17 @@ import io.netty.buffer.ByteBuf;
  *
  * @author beykery
  */
-public interface Output
-{
+public interface Output {
 
-  /**
-   * kcp的底层输出
-   *
-   * @param msg 消息
-   * @param kcp kcp对象
-   * @param user 远端地址
-   */
-  void out(ByteBuf msg, Kcp kcp, Object user);
+	/**
+	 * kcp的底层输出
+	 *
+	 * @param msg
+	 *            消息
+	 * @param kcp
+	 *            kcp对象
+	 * @param user
+	 *            远端地址
+	 */
+	void out(ByteBuf msg, Kcp kcp, Object user);
 }
